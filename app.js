97e8f7c751843302e6b4b7d21beccaf748d58d28
Lindsay Urchyk <urchykli@gmail.com>
2020-01-30 13:39:34 -0500

    Adjust link paths

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 // let csv = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTtLJIlrB1oAyaQXY6jAlsinmptuHZClR-d8kOzXbv9xSLyTYl-jFGmt92wmAvQ9qq64Ewps-tHAeaO/pub?gid=1716883814&single=true&output=csv'
 let csv = './data.csv'
 
-// let data = d3.csv(csv).then(function (data) {
-//   console.log(data)
-//   return data
-// })
-
 const margin = { top: 40, right: 10, bottom: 10, left: 10 }
-const fullWidth = 1500
+const fullWidth = 1700
 const fullHeight = 1000
 const width = fullWidth - margin.left - margin.right
 const height = fullHeight - margin.top - margin.bottom
@@ -42,30 +37,25 @@ async function parseData(csv) {
       relationship.push({ 'name': childMissile, 'parent': parentMissile, 'year': missiles[5], 'inPossession': missiles[6], 'range': missiles[7], 'url': missiles[8], 'annotation': missiles[9] })
     })
     let stratify = d3.stratify()
-      .id(function (d) { return d.name })
-      .parentId(function (d) { return d.parent })
+      .id(d => d.name)
+      .parentId(d => d.parent)
     let root = stratify(relationship)
     return root
   })
   return d3.hierarchy(nodes)
 }
 
-// async function yearsArray() {
-//   let years = await fetchCSV(csv)
-// }
-
 async function createTree() {
 
-  let data = await parseData(csv)
-  let findYears = await fetchCSV(csv)
+  const data = await parseData(csv)
+  const findYears = await fetchCSV(csv)
   let years = []
   findYears.forEach(missiles => {
     years.push(missiles[5])
   })
 
-
-  let minYear = d3.min(years)
-  let maxYear = d3.max(years)
+  const minYear = d3.min(years)
+  const maxYear = d3.max(years)
 
   let y_scale = d3.scaleLinear()
     .domain([minYear, maxYear])
@@ -87,22 +77,21 @@ async function createTree() {
 
   const g = svg.append('g')
     .attr('transform', `translate(${margin.left},${margin.top})`)
+
+  g.append('g')
     .call(y_axis)
-
-  g.append('text')
-    .text('d3.tree - A Family Tree')
-    .attr('class', 'title')
-    .attr('x', 50)
-    .attr('y', 50)
-
 
   g.selectAll(".tick text")
     .attr('font-size', '20')
     .attr("transform", `translate(${margin.left + 50},${0})`)
 
-  const elbow = (d, i) => {
-    return `M${d.source.x}, ${y_scale(d.source.data.data.year)} H${d.target.x}, V${y_scale(d.target.data.data.year)} ${d.target.children ? '' : 'h' + margin.right} `
-  }
+  // const elbow = (d, i) => {
+  //   return `M${d.target.x}, ${y_scale(d.target.data.data.year)} C${(d.target.x + d.source.x) / 2},${y_scale(d.target.data.data.year)} ${(d.target.x + d.source.x) / 2},${y_scale(d.source.data.data.year)} ${d.source.x},${y_scale(d.source.data.data.year)}`
+  // }
+
+  let linkPath = d3.linkHorizontal()
+    .x(d => d.x)
+    .y(d => y_scale(d.data.data.year))
 
   let treeNodes = tree(data)
 
@@ -110,21 +99,32 @@ async function createTree() {
     .data(treeNodes.links())
     .enter().append('path')
     .attr('class', 'link')
-    .attr('d', elbow)
+    .attr("fill", "none")
+    .attr("stroke", "Red")
+    .attr("d", linkPath)
 
-
-  const node = svg.selectAll('.node')
+  const node = g.selectAll('.node')
     .data(treeNodes.descendants())
     .enter().append('g')
     .attr('class', 'node')
-    .attr('transform', d => {
-      return "translate(" + d.x + "," + y_scale(d.data.data.year) + ")"
-    })
+    .attr('transform', d => "translate(" + d.x + "," + y_scale(d.data.data.year) + ")")
+
+  node.append("circle")
+    .attr("r", 5)
+  // .style("stroke", d => d.data.type)
+  // .style("fill", d => d.data.level)
 
   node.append('text')
-    .attr('x', 8)
-    .attr('y', -6)
+    // .attr('x', 0)
+    .attr('y', 25)
+    .attr('text-anchor', 'middle')
     .text(d => `${d.data.id.split(",")[0]} `)
+    .attr('class', 'text')
+
+  node.append('text')
+    .attr('y', 40)
+    .attr('text-anchor', 'middle')
+    .text(d => `${d.data.id.split(",")[1]} `)
     .attr('class', 'text')
 }
 function fetchCSV(src) {
